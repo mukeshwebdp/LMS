71a8c69199d2a6837ae0e0ab3ba7e1e1266d17bd
mukeshwebdp <mukesh.webdp@gmail.com>
2025-02-08 17:16:57 +0530

    add courseRoutes middleware

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-import userRoutes from './routes/user.routes.js'
+import userRoutes from './routes/user.routes.js';
+import courseRoutes from './routes/course.routes.js';
 const app = express()
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     credentials: true
@@ -16,6 +18,8 @@ app.use('/ping',function(req,res){
     res.send('/pong');
 })
 app.use('/api/v1/user',userRoutes)
+app.use('/api/v1/courses',courseRoutes)
+
 app.all('*',(req,res)=>{
     
     res.status(404).send('OOPS!! 404 page not found');
